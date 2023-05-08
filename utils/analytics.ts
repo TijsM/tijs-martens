ab8537056337df29a13e834feb8ea9f3f7828e25
TijsM <tijs.martens.tijs@gmail.com>
2023-05-08 09:53:11 +0200

    feat: add ga4

--- utils/analytics.ts
+++ utils/analytics.ts
@@ -1,34 +1,39 @@
 import { useEffect } from "react";
-// import ReactGA from "react-ga4";
+import ReactGA from "react-ga4";
 import posthog from "posthog-js";
 
 export type AnalyticsData = {
-  properties?: { [data: string]: any };
   action: "click" | "view";
   name: string;
+  properties?: { [data: string]: any };
 };
 
-export const initGA = () => {
-  console.log(process.env.POSTHOG_ID as string, {
-    api_host: process.env.POSTHOG_API_HOST,
-  })
+export const initAnalytics = () => {
   posthog.init(process.env.POSTHOG_ID as string, {
     api_host: process.env.POSTHOG_API_HOST,
   });
+
+  ReactGA.initialize([
+    { trackingId: process.env.GOOGLE_ANALYTICS_ID as string },
+  ]);
 };
 
 export const logPageView = () => {
   posthog.capture("view_page", { pageName: window.location.pathname });
+  ReactGA.send({ hitType: "pageview", page: window.location.pathname });
 };
 
 export const logEvent = (data: AnalyticsData) => {
-  console.log(data.action + "_" + data.name, { ...data.properties })
   posthog.capture(data.action + "_" + data.name, { ...data.properties });
+  ReactGA.event({
+    category: data.action,
+    action: data.action + " - " + data.name,
+  });
 };
 
 export const useLogPageView = () => {
   useEffect(() => {
-    initGA();
+    initAnalytics();
     logPageView();
   }, []);
 };
