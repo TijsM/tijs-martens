54179e726f88656fb86c15faf23f4b3d4daf901a
TijsM <tijs.martens.tijs@gmail.com>
2023-05-18 10:07:10 +0200

    fix: remove log

--- components/LinkItem.loading.tsx
+++ components/LinkItem.loading.tsx
@@ -2,7 +2,6 @@ import styled from "styled-components";
 import { StArticle, Title } from "./Types/ComponentTypes";
 
 export const LinkItemLoader = () => {
-  console.log("rendering");
   return (
     <StContainer>
       <StTitle />
