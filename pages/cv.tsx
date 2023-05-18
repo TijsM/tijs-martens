fdf5835e05ae9bea2a5bdeb7291bc03816311130
TijsM <tijs.martens.tijs@gmail.com>
2023-05-18 12:28:33 +0200

    feat: add cv redirect

---
+++ pages/cv.tsx
@@ -0,0 +1,41 @@
+import Head from "next/head";
+
+import styled from "styled-components";
+
+import { useLogPageView } from "../utils/analytics";
+import { useEffect } from "react";
+
+export default function Home() {
+  useLogPageView();
+
+  useEffect(() => {
+    window.location.href =
+      "https://robust-morning-27f.notion.site/CV-899b03301c894df68055e5161e856d6e";
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <title>Tijs Martens</title>
+        <meta
+          name="description"
+          content="Full stack developer with a product passion based in Ghent, Belgium."
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <StMain>redirecting</StMain>
+    </>
+  );
+}
+
+const StMain = styled.main`
+  background-color: black;
+  height: 100vh;
+  padding: 16px;
+  overflow-y: scroll;
+
+  @media only screen and (min-width: 768px) {
+    padding: 16px 256px;
+  }
+`;
