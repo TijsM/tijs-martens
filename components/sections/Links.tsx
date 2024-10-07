3507dd68acbe22322ea3c02d10762e3bd6c71709
Tijs Martens <tijs.martens.tijs@gmail.com>
2024-10-07 10:08:16 +0200

    feat: add tracking on links

--- components/sections/Links.tsx
+++ components/sections/Links.tsx
@@ -1,17 +1,51 @@
 import styled from "styled-components";
 import { StSection } from "../styles/StSection";
+import { logEvent } from "../../utils/analytics";
 
 export const Links = () => {
   return (
     <StContainer>
       <StSection>
-        <StLink href={"https://www.linkedin.com/in/tijsm"} target="_blank">
+        <StLink
+          onClick={() => {
+            logEvent({
+              action: "click",
+              name: "open_link_from_header",
+              properties: {
+                urlName: "LinkedIn",
+              },
+            });
+          }}
+          href={"https://www.linkedin.com/in/tijsm"}
+          target="_blank"
+        >
           LinkedIn
         </StLink>
-        <StLink href={"https://github.com/TijsM"} target="_blank">
+        <StLink
+          onClick={() => {
+            logEvent({
+              action: "click",
+              name: "open_link_from_header",
+              properties: {
+                urlName: "GitHub",
+              },
+            });
+          }}
+          href={"https://github.com/TijsM"}
+          target="_blank"
+        >
           GitHub
         </StLink>
         <StLink
+          onClick={() => {
+            logEvent({
+              action: "click",
+              name: "open_link_from_header",
+              properties: {
+                urlName: "Strava",
+              },
+            });
+          }}
           href={"https://www.strava.com/athletes/48852110"}
           target="_blank"
         >
