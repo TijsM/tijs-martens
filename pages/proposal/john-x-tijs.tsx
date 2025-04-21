32df2f03478f51195d10ba5c9539a3b525612729
Tijs Martens <tijs.martens.tijs@gmail.com>
2025-04-21 08:25:05 +0200

    feat: add proposal

---
+++ pages/proposal/john-x-tijs.tsx
@@ -0,0 +1,41 @@
+import Head from "next/head";
+
+import styled from "styled-components";
+
+import { useLogPageView } from "../../utils/analytics";
+import { useEffect } from "react";
+
+export default function Home() {
+  useLogPageView();
+
+  useEffect(() => {
+    window.location.href =
+      "https://tijs-martens.notion.site/John-X-Tijs-1da0d060bfa680a9892cc373bd2ec13b?pvs=74";
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <title>Tijs Martens</title>
+        <meta
+          name="description"
+          content="Full stack developer with a product passion based in Ghent, Belgium."
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <StMain>redirecting</StMain>
+    </>
+  );
+}
+
+const StMain = styled.main`
+  background-color: black;
+  height: 100vh;
+  padding: 16px;
+  overflow-y: scroll;
+
+  @media only screen and (min-width: 768px) {
+    padding: 16px 256px;
+  }
+`;
